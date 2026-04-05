449bcf8bc76affd09293ce80a1d3e44a2dda2043
azyl <hardsecur687@gmail.com>
2026-04-05 08:26:07 +0200

    Add files via upload

---
+++ src/api/vite.config.js
@@ -0,0 +1,19 @@
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import path from 'path'
+
+// https://vite.dev/config/
+export default defineConfig({
+  logLevel: 'info',
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  preview: {
+    allowedHosts: ['longdrive.onrender.com'],
+    host: '0.0.0.0',
+    port: 10000
+  }
+})
